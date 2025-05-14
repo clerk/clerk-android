07aaec4c20c4247864b79ef390f4f0a4c5827f3c
Sam Wolfand <sam.wolfand@gmail.com>
2025-05-14 12:50:08 -0700

    add publishing

--- map-generator/build.gradle.kts
+++ map-generator/build.gradle.kts
@@ -2,11 +2,27 @@ plugins {
   id("java-library")
   alias(libs.plugins.jetbrains.kotlin.jvm)
   alias(libs.plugins.ksp)
+  id("maven-publish")
 }
 
 java {
   sourceCompatibility = JavaVersion.VERSION_21
   targetCompatibility = JavaVersion.VERSION_21
+
+  withSourcesJar()
+  withJavadocJar()
+}
+
+publishing {
+  publications {
+    register<MavenPublication>("mapGenerator") {
+      groupId = "com.clerk"
+      artifactId = "map-generator"
+      version = "0.1.0"
+
+      from(components["java"])
+    }
+  }
 }
 
 kotlin { compilerOptions { jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21 } }
