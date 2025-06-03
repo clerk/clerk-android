6c93331afc110f1c5333c92cc39a161893f671e6
Sam Wolfand <sam.wolfand@gmail.com>
2025-06-03 14:25:59 -0700

    fix oauth

--- source/src/main/kotlin/com/clerk/signin/SignIn.kt
+++ source/src/main/kotlin/com/clerk/signin/SignIn.kt
@@ -439,11 +439,20 @@ data class SignIn(
        *
        * @param strategy The strategy identifier for ticket authentication.
        * @param identifier The identifier for the ticket authentication.
+       * @param ticket The ticket used for authentication.
+       * **
        */
       @AutoMap
       @Serializable
-      data class Ticket(override val strategy: String = TICKET, val identifier: String) : Strategy {
-        constructor(identifier: String) : this(strategy = TICKET, identifier = identifier)
+      data class Ticket(
+        override val strategy: String = TICKET,
+        val identifier: String,
+        val ticket: String,
+      ) : Strategy {
+        constructor(
+          identifier: String,
+          ticket: String,
+        ) : this(strategy = TICKET, identifier = identifier, ticket = ticket)
       }
     }
   }
