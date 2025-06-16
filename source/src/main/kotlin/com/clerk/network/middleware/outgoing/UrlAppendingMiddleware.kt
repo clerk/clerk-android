22e4f04575b7da97f8e6ee4361ef612791893b4e
Sam Wolfand <sam.wolfand@gmail.com>
2025-06-16 01:45:51 -0700

    Update ClerkApiResultConverterFactory.kt and add ClerkApiResultConverterFactoryTest

--- source/src/main/kotlin/com/clerk/network/middleware/outgoing/UrlAppendingMiddleware.kt
+++ source/src/main/kotlin/com/clerk/network/middleware/outgoing/UrlAppendingMiddleware.kt
@@ -1,6 +1,5 @@
 package com.clerk.network.middleware.outgoing
 
-import com.clerk.log.ClerkLog
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -13,8 +12,6 @@ internal class UrlAppendingMiddleware : Interceptor {
 
     val appendedUrl =
       originalUrl.newBuilder().addQueryParameter(IS_NATIVE_QUERY_PARAM, true.toString()).build()
-
-    ClerkLog.e("QQQ: $appendedUrl")
 
     val newRequest = originalRequest.newBuilder().url(appendedUrl).build()
     return chain.proceed(newRequest)
--- source/src/main/kotlin/com/clerk/network/serialization/ClerkApiResultConverterFactory.kt
+++ source/src/main/kotlin/com/clerk/network/serialization/ClerkApiResultConverterFactory.kt
@@ -1,5 +1,6 @@
 package com.clerk.network.serialization
 
+import com.clerk.log.ClerkLog
 import com.clerk.network.model.response.ClientPiggybackedResponse
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
@@ -16,20 +17,52 @@ internal object ClerkApiResultConverterFactory : Converter.Factory() {
     annotations: Array<out Annotation>,
     retrofit: Retrofit,
   ): Converter<ResponseBody, *>? {
-    if (getRawType(type) != ClerkResult::class.java) return null
+    ClerkLog.d("ClerkApiResultConverterFactory.responseBodyConverter called with type: $type")
+
+    if (getRawType(type) != ClerkResult::class.java) {
+      ClerkLog.d("Type is not ClerkResult, returning null")
+      return null
+    }
+
+    ClerkLog.d("Type is ClerkResult, proceeding with conversion")
 
     val successType = (type as ParameterizedType).actualTypeArguments[0]
     val errorType = type.actualTypeArguments[1]
+
+    ClerkLog.d("Success type: $successType")
+    ClerkLog.d("Error type: $errorType")
+
     val errorResultType: Annotation = createResultType(errorType)
     val nextAnnotations = annotations.toList() + errorResultType
 
-    // Check if the success type should be wrapped in ClientPiggybackedResponse
+    // For List<Session>, don't wrap in ClientPiggybackedResponse - it comes as plain JSON array
+    val rawSuccessType = getRawType(successType)
+    ClerkLog.d("Raw success type: $rawSuccessType")
+
+    val isListType =
+      rawSuccessType == List::class.java ||
+        (successType.toString().contains("java.util.List") &&
+          successType.toString().contains("Session"))
+
     val actualSuccessType =
-      if (shouldWrapInClientPiggybackedResponse(successType)) {
-        createParameterizedType(ClientPiggybackedResponse::class.java, successType)
-      } else {
+      if (isListType) {
+        ClerkLog.d(
+          "This is a List<Session> type, using direct type (no ClientPiggybackedResponse wrapper)"
+        )
         successType
+      } else {
+        // Check if the success type should be wrapped in ClientPiggybackedResponse
+        val shouldWrap = shouldWrapInClientPiggybackedResponse(successType)
+        ClerkLog.d("Should wrap in ClientPiggybackedResponse: $shouldWrap")
+
+        if (shouldWrap) {
+          createParameterizedType(ClientPiggybackedResponse::class.java, successType)
+        } else {
+          successType
+        }
       }
+
+    ClerkLog.d("Actual success type for conversion: $actualSuccessType")
 
     val delegateConverter =
       retrofit.nextResponseBodyConverter<Any>(
@@ -37,13 +70,22 @@ internal object ClerkApiResultConverterFactory : Converter.Factory() {
         actualSuccessType,
         nextAnnotations.toTypedArray(),
       )
+
+    ClerkLog.d("Delegate converter: $delegateConverter")
     return ClerkApiResultConverter(delegateConverter)
   }
 
   private fun shouldWrapInClientPiggybackedResponse(successType: Type): Boolean {
-    // Check if this is a type that should be unwrapped from ClientPiggybackedResponse
     val rawType = getRawType(successType)
-    return rawType.name != ENVIRONMENT_TYPE
+
+    // Don't wrap the Environment type
+    if (rawType.name == ENVIRONMENT_TYPE) {
+      ClerkLog.d("Not wrapping Environment type in ClientPiggybackedResponse")
+      return false
+    }
+
+    ClerkLog.d("Wrapping type in ClientPiggybackedResponse")
+    return true
   }
 
   private fun createParameterizedType(rawType: Class<*>, typeArgument: Type): ParameterizedType {
@@ -59,16 +101,28 @@ internal object ClerkApiResultConverterFactory : Converter.Factory() {
   private class ClerkApiResultConverter(private val delegate: Converter<ResponseBody, Any>) :
     Converter<ResponseBody, ClerkResult<*, *>> {
     override fun convert(value: ResponseBody): ClerkResult<*, *>? {
+      ClerkLog.d("ClerkApiResultConverter.convert called")
+
       return delegate.convert(value)?.let { result ->
+        ClerkLog.d("Delegate converter returned: $result")
+
         // If the result is a ClientPiggybackedResponse, unwrap it
         val unwrappedResult =
           if (result is ClientPiggybackedResponse<*>) {
+            ClerkLog.d("Unwrapping ClientPiggybackedResponse")
             result.response
           } else {
+            ClerkLog.d("Result is not ClientPiggybackedResponse, using as-is")
             result
           }
+
+        ClerkLog.d("Final unwrapped result: $unwrappedResult")
         @Suppress("UNCHECKED_CAST") ClerkResult.success(unwrappedResult as Any)
       }
+        ?: run {
+          ClerkLog.e("Delegate converter returned null!")
+          null
+        }
     }
   }
 }
