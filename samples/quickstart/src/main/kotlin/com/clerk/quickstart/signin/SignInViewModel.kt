8b6e4a93528285718108c73186f60962c8ae949a
Sam Wolfand <sam.wolfand@gmail.com>
2025-07-17 20:30:41 -0700

    Add SignInViewModel.kt

---
+++ samples/quickstart/src/main/kotlin/com/clerk/quickstart/signin/SignInViewModel.kt
@@ -0,0 +1,31 @@
+package com.clerk.quickstart.signin
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.clerk.network.serialization.longErrorMessageOrNull
+import com.clerk.network.serialization.onFailure
+import com.clerk.network.serialization.onSuccess
+import com.clerk.signin.SignIn
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class SignInViewModel : ViewModel() {
+  private val _uiState = MutableStateFlow<SignInUiState>(SignInUiState.SignedOut)
+  val uiState = _uiState.asStateFlow()
+
+  fun signIn(email: String, password: String) {
+    viewModelScope.launch {
+      SignIn.create(SignIn.CreateParams.Strategy.Password(identifier = email, password = password))
+        .onSuccess { _uiState.value = SignInUiState.SignedIn }
+        .onFailure { Log.e("SignInViewModel", "${it.longErrorMessageOrNull}", it.throwable) }
+    }
+  }
+}
+
+sealed interface SignInUiState {
+  data object SignedOut : SignInUiState
+
+  data object SignedIn : SignInUiState
+}
