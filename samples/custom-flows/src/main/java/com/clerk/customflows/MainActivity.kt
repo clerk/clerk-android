ddd230c924e67c45691f06d3d48f4786c3378bc1
Sam Wolfand <sam.wolfand@gmail.com>
2025-07-21 18:44:42 -0700

    add entry

--- samples/custom-flows/src/main/java/com/clerk/customflows/MainActivity.kt
+++ samples/custom-flows/src/main/java/com/clerk/customflows/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.clerk.customflows
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.clerk.customflows.emailpassword.signin.EmailPasswordSignInActivity
 import com.clerk.customflows.ui.theme.ClerkTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +29,24 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
+      val context = LocalContext.current
       ClerkTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
+          Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding).padding(horizontal = 24.dp),
+            verticalArrangement =
+              Arrangement.spacedBy(24.dp, alignment = Alignment.CenterVertically),
+          ) {
+            Button(
+              shape = RoundedCornerShape(8.dp),
+              modifier = Modifier.fillMaxWidth().height(48.dp),
+              onClick = {
+                context.startActivity(Intent(context, EmailPasswordSignInActivity::class.java))
+              },
+            ) {
+              Text("Email & Password Sign In")
+            }
+          }
         }
       }
     }
