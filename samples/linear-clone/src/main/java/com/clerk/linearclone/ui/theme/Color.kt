6a7f685cd29b5aa2fc20cd51e519f9b1b286d089
Sam Wolfand <sam.wolfand@gmail.com>
2025-07-25 17:52:44 -0700

    update theme

--- samples/linear-clone/src/main/java/com/clerk/linearclone/ui/theme/Color.kt
+++ samples/linear-clone/src/main/java/com/clerk/linearclone/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package com.clerk.linearclone.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
+val PrimaryGrey = Color(0xFF2A2A2A)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
