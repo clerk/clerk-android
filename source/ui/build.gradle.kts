ba88016ad25827218e25ae2f31df4d2954da53d7
Sam Wolfand <sam.wolfand@gmail.com>
2025-08-07 21:35:54 -0700

    add new module

---
+++ source/ui/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.android.library)
+}
+
+android {
+  namespace = "com.clerk.ui"
+  compileSdk = 36
+
+  defaultConfig {
+    minSdk = 24
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = true
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlin { compilerOptions { jvmTarget = JvmTarget.JVM_17 } }
+}
+
+dependencies {
+  implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.material)
+
+  testImplementation(libs.junit)
+
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(libs.androidx.junit)
+}
