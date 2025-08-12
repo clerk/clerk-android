2a9a6907f983f2999d8dab2adacca7c528856ee4
Sam Wolfand <sam.wolfand@gmail.com>
2025-08-12 14:36:48 -0700

    Sam/mobile 121 android universal button component (#172)
    * remove hilt
    
    * some cleanup
    
    * add tests for ColorUtils.kt
    
    * A lot of refactoring for clerk theme
    
    * moved compose into api module and cleaned up

--- source/api/src/main/kotlin/com/clerk/api/ui/ClerkDesign.kt
+++ source/api/src/main/kotlin/com/clerk/api/ui/ClerkDesign.kt
@@ -1,8 +1,11 @@
 package com.clerk.api.ui
 
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
 /**
  * Represents the design configuration for Clerk UI components.
  *
  * @property borderRadius The corner radius for the design elements.
  */
-data class ClerkDesign(val borderRadius: Float)
+data class ClerkDesign(val borderRadius: Dp = 6.dp)
