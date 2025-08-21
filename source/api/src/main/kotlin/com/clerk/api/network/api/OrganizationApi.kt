49d482a9cb8ce620d1d38cb4132e74bc82e716f2
Sam Wolfand <sam.wolfand@gmail.com>
2025-08-21 17:55:12 -0400

    add roles api

--- source/api/src/main/kotlin/com/clerk/api/network/api/OrganizationApi.kt
+++ source/api/src/main/kotlin/com/clerk/api/network/api/OrganizationApi.kt
@@ -1,9 +1,12 @@
 package com.clerk.api.network.api
 
+import com.clerk.api.network.model.error.ClerkErrorResponse
+import com.clerk.api.network.model.roles.Role
 import com.clerk.api.network.paths.Organizations
+import com.clerk.api.network.serialization.ClerkResult
 import retrofit2.http.GET
 
 interface OrganizationApi {
 
-  @GET(Organizations.Roles.GET_ROLES) fun getRoles(): List<Role>
+  @GET(Organizations.Roles.GET_ROLES) fun getRoles(): ClerkResult<List<Role>, ClerkErrorResponse>
 }
