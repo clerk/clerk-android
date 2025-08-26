27e91577cbb47963fc0bde50be0d8dcaeadbdd52
Sam Wolfand <sam.wolfand@gmail.com>
2025-08-26 10:28:35 -0700

    update build.gradle.kts

--- workbench/build.gradle.kts
+++ workbench/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.android)
@@ -20,7 +22,7 @@ android {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
   }
-  kotlinOptions { jvmTarget = "11" }
+  kotlin { compilerOptions { jvmTarget.set(JvmTarget.JVM_17) } }
   buildFeatures { compose = true }
 }
 
