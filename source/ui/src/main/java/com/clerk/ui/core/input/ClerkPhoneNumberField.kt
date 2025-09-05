2b3acd7bc7a25bff1125473a973de600f2f0e1af
Sam Wolfand <sam@clerk.dev>
2025-09-05 14:31:20 -0700

    add autofill (#223)

--- source/ui/src/main/java/com/clerk/ui/core/input/ClerkPhoneNumberField.kt
+++ source/ui/src/main/java/com/clerk/ui/core/input/ClerkPhoneNumberField.kt
@@ -33,11 +33,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.ContentType
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentType
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.PreviewLightDark
@@ -145,6 +148,7 @@ private fun PhoneNumberInput(
 
   Column {
     OutlinedTextField(
+      modifier = Modifier.semantics { contentType = ContentType.PhoneNumber },
       colors =
         OutlinedTextFieldDefaults.colors(
           unfocusedBorderColor = computedColors.inputBorder,
