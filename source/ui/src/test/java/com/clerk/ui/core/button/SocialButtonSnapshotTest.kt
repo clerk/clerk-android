b7d8ab93cd6ecffc83b48fb477c3f9e467e913d8
Sam Wolfand <sam.wolfand@gmail.com>
2025-09-08 15:10:59 -0700

    update snapshot tests and button to respect size

--- source/ui/src/test/java/com/clerk/ui/core/button/SocialButtonSnapshotTest.kt
+++ source/ui/src/test/java/com/clerk/ui/core/button/SocialButtonSnapshotTest.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.clerk.api.Clerk
 import com.clerk.api.sso.OAuthProvider
 import com.clerk.api.sso.setLogoUrl
@@ -49,6 +51,46 @@ class SocialButtonSnapshotTest : BaseSnapshotTest() {
       ClerkMaterialTheme {
         Column(
           Modifier.fillMaxSize().background(ClerkMaterialTheme.colors.background).padding(dp12),
+          horizontalAlignment = Alignment.CenterHorizontally,
+          verticalArrangement = Arrangement.spacedBy(dp12, Alignment.CenterVertically),
+        ) {
+          ClerkSocialButton(provider = provider)
+          ClerkSocialButton(provider = provider, isPressed = true)
+          ClerkSocialButton(provider = provider, isEnabled = false)
+        }
+      }
+    }
+  }
+
+  @Test
+  fun socialButtonSnapshotTestDarkShortForm() {
+    Clerk.customTheme = ClerkTheme(colors = DefaultColors.dark)
+    val provider = OAuthProvider.GOOGLE
+    provider.setLogoUrl(null)
+    paparazzi.snapshot {
+      ClerkMaterialTheme {
+        Column(
+          Modifier.width(200.dp).background(ClerkMaterialTheme.colors.background).padding(dp12),
+          horizontalAlignment = Alignment.CenterHorizontally,
+          verticalArrangement = Arrangement.spacedBy(dp12, Alignment.CenterVertically),
+        ) {
+          ClerkSocialButton(provider = provider)
+          ClerkSocialButton(provider = provider, isPressed = true)
+          ClerkSocialButton(provider = provider, isEnabled = false)
+        }
+      }
+    }
+  }
+
+  @Test
+  fun socialButtonSnapshotTestLightShortForm() {
+    Clerk.customTheme = ClerkTheme(colors = DefaultColors.light)
+    val provider = OAuthProvider.GOOGLE
+    provider.setLogoUrl(null)
+    paparazzi.snapshot {
+      ClerkMaterialTheme {
+        Column(
+          Modifier.width(200.dp).background(ClerkMaterialTheme.colors.background).padding(dp12),
           horizontalAlignment = Alignment.CenterHorizontally,
           verticalArrangement = Arrangement.spacedBy(dp12, Alignment.CenterVertically),
         ) {
