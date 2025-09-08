a46edc2601313bb5ba2064e238479119c317d023
Sam Wolfand <sam@clerk.dev>
2025-09-08 14:07:12 -0700

    Add code input field (#224)
    * Add otp box
    
    * add snapshot tests
    
    * rename
    
    * update ClerkCodeInputField.kt to have error states and supporting text
    
    * update snapshots

---
+++ source/ui/src/test/java/com/clerk/ui/core/input/ClerkCodeInputFieldSnapshotTest.kt
@@ -0,0 +1,82 @@
+package com.clerk.ui.core.input
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import com.clerk.api.Clerk
+import com.clerk.api.ui.ClerkTheme
+import com.clerk.base.BaseSnapshotTest
+import com.clerk.ui.core.dimens.dp16
+import com.clerk.ui.theme.ClerkMaterialTheme
+import com.clerk.ui.theme.DefaultColors
+import org.junit.Test
+
+class ClerkCodeInputFieldSnapshotTest : BaseSnapshotTest() {
+
+  @Test
+  fun testClerkCodeInputField() {
+    paparazzi.snapshot {
+      ClerkMaterialTheme {
+        Column(
+          modifier = Modifier.background(ClerkMaterialTheme.colors.background).padding(dp16),
+          verticalArrangement = Arrangement.spacedBy(dp16),
+        ) {
+          ClerkCodeInputField(onOtpTextChange = {}, secondsLeft = 30, onClickResend = {})
+          ClerkCodeInputField(
+            onOtpTextChange = {},
+            isError = true,
+            secondsLeft = 30,
+            onClickResend = {},
+          )
+          ClerkCodeInputField(
+            onOtpTextChange = {},
+            isSuccess = true,
+            secondsLeft = 0,
+            onClickResend = {},
+          )
+          ClerkCodeInputField(
+            onOtpTextChange = {},
+            isVerifying = true,
+            secondsLeft = 0,
+            onClickResend = {},
+          )
+        }
+      }
+    }
+  }
+
+  @Test
+  fun testClerkCodeInputFieldDarkMode() {
+    Clerk.customTheme = ClerkTheme(colors = DefaultColors.dark)
+    paparazzi.snapshot {
+      ClerkMaterialTheme {
+        Column(
+          modifier = Modifier.background(ClerkMaterialTheme.colors.background).padding(dp16),
+          verticalArrangement = Arrangement.spacedBy(dp16),
+        ) {
+          ClerkCodeInputField(onOtpTextChange = {}, secondsLeft = 30, onClickResend = {})
+          ClerkCodeInputField(
+            onOtpTextChange = {},
+            isError = true,
+            secondsLeft = 30,
+            onClickResend = {},
+          )
+          ClerkCodeInputField(
+            onOtpTextChange = {},
+            isSuccess = true,
+            secondsLeft = 0,
+            onClickResend = {},
+          )
+          ClerkCodeInputField(
+            onOtpTextChange = {},
+            isVerifying = true,
+            secondsLeft = 0,
+            onClickResend = {},
+          )
+        }
+      }
+    }
+  }
+}
