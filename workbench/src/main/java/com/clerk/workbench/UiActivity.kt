d4195a6db7a3463126b0d5905c3347328149d5e2
Sam Wolfand <sam@clerk.dev>
2025-09-12 16:43:43 -0700

    Add SignInGetHelpView (#244)
    * Add ClerkScaffold.kt
    
    * replace authheader with scaffold
    
    * make code view use scaffold
    
    * add email to and make error snackbar common
    
    * add secured by clerk, make button full width
    
    * add snapshot test

--- workbench/src/main/java/com/clerk/workbench/UiActivity.kt
+++ workbench/src/main/java/com/clerk/workbench/UiActivity.kt
@@ -5,8 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import com.clerk.api.network.model.factor.Factor
-import com.clerk.ui.signin.passkey.SignInFactorOnePasskeyView
+import com.clerk.ui.signin.help.SignInGetHelpView
 import com.clerk.workbench.ui.theme.WorkbenchTheme
 
 class UiActivity : ComponentActivity() {
@@ -18,9 +17,7 @@ class UiActivity : ComponentActivity() {
 
 @Composable
 private fun MainContent() {
-  SignInFactorOnePasskeyView(
-    factor = Factor(strategy = "passkey", safeIdentifier = "sam@clerk.dev")
-  )
+  SignInGetHelpView {}
 }
 
 @PreviewLightDark
