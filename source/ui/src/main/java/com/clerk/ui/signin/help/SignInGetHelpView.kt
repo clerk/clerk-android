cf0f850b525de853e642a171d0a4bf7f58082641
Sam Wolfand <sam.wolfand@gmail.com>
2025-09-12 16:31:30 -0700

    add secured by clerk, make button full width

--- source/ui/src/main/java/com/clerk/ui/signin/help/SignInGetHelpView.kt
+++ source/ui/src/main/java/com/clerk/ui/signin/help/SignInGetHelpView.kt
@@ -2,11 +2,12 @@ package com.clerk.ui.signin.help
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -16,12 +17,16 @@ import com.clerk.api.log.ClerkLog
 import com.clerk.ui.R
 import com.clerk.ui.core.button.standard.ClerkButton
 import com.clerk.ui.core.common.ClerkAuthScaffold
+import com.clerk.ui.core.common.SecuredByClerk
+import com.clerk.ui.core.common.Spacers
 import com.clerk.ui.core.error.ClerkErrorSnackbar
 import com.clerk.ui.theme.ClerkMaterialTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun SignInGetHelpView(modifier: Modifier = Modifier, onBackPressed: () -> Unit = {}) {
   val snackbarHostState = remember { SnackbarHostState() }
+  val scope = rememberCoroutineScope()
   ClerkMaterialTheme {
     ClerkAuthScaffold(
       snackbarHost = { ClerkErrorSnackbar(snackbarHostState) },
@@ -36,19 +41,26 @@ fun SignInGetHelpView(modifier: Modifier = Modifier, onBackPressed: () -> Unit =
           data = "mailto:".toUri()
           putExtra(Intent.EXTRA_EMAIL, arrayOf(stringResource(R.string.support_clerk_com)))
         }
-      try {
-        context.startActivity(Intent.createChooser(emailIntent, "Contact Clerk Support"))
-      } catch (_: ActivityNotFoundException) {
-        ClerkLog.e("No email clients installed on device.")
-        LaunchedEffect(Unit) {
-          snackbarHostState.showSnackbar(
-            message = context.getString(R.string.no_email_clients_installed_on_device),
-            duration = SnackbarDuration.Short,
-          )
-        }
-      }
 
-      ClerkButton(text = stringResource(R.string.email_support), onClick = {})
+      ClerkButton(
+        modifier = Modifier.fillMaxWidth(),
+        text = stringResource(R.string.email_support),
+        onClick = {
+          try {
+            context.startActivity(Intent.createChooser(emailIntent, "Contact Clerk Support"))
+          } catch (_: ActivityNotFoundException) {
+            ClerkLog.e("No email clients installed on device.")
+            scope.launch {
+              snackbarHostState.showSnackbar(
+                message = context.getString(R.string.no_email_clients_installed_on_device),
+                duration = SnackbarDuration.Short,
+              )
+            }
+          }
+        },
+      )
+      Spacers.Vertical.Spacer32()
+      SecuredByClerk()
     }
   }
 }
