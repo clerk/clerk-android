60fd622cbfc06e9b094e44b589ca30dd76d05cc8
Sam Wolfand <sam.wolfand@gmail.com>
2025-09-16 19:26:33 -0700

    add kdoc

--- source/ui/src/main/java/com/clerk/ui/signin/SignInFactorTwoView.kt
+++ source/ui/src/main/java/com/clerk/ui/signin/SignInFactorTwoView.kt
@@ -9,6 +9,17 @@ import com.clerk.ui.signin.backupcode.SignInFactorTwoBackupCodeView
 import com.clerk.ui.signin.code.SignInFactorCodeView
 import com.clerk.ui.signin.help.SignInGetHelpView
 
+/**
+ * A composable that acts as a router for displaying the appropriate second-factor authentication
+ * view.
+ *
+ * Based on the `strategy` of the provided [factor], this composable will delegate rendering to the
+ * corresponding view, such as [SignInFactorCodeView] for TOTP or phone code, or
+ * [SignInFactorTwoBackupCodeView] for backup codes.
+ *
+ * @param factor The second factor to be verified.
+ * @param modifier The [Modifier] to be applied to the view.
+ */
 @Composable
 fun SignInFactorTwoView(factor: Factor, modifier: Modifier = Modifier) {
   when (factor.strategy) {
