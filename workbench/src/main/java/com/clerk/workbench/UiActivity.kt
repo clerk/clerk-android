2b2af32c502484d23617d924198943886a2a6003
Sam Wolfand <sam.wolfand@gmail.com>
2025-09-18 16:52:55 -0700

    Some cleanup

--- workbench/src/main/java/com/clerk/workbench/UiActivity.kt
+++ workbench/src/main/java/com/clerk/workbench/UiActivity.kt
@@ -3,26 +3,38 @@ package com.clerk.workbench
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.clerk.ui.auth.AuthMode
 import com.clerk.ui.auth.AuthStartView
 import com.clerk.workbench.ui.theme.WorkbenchTheme
 
 class UiActivity : ComponentActivity() {
+  val viewModel: MainViewModel by viewModels()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent { WorkbenchTheme { MainContent() } }
+    setContent { WorkbenchTheme { MainContent(viewModel) } }
   }
 }
 
 @Composable
-private fun MainContent() {
-  AuthStartView(authMode = AuthMode.SignIn)
+private fun MainContent(viewModel: MainViewModel) {
+  val state by viewModel.uiState.collectAsStateWithLifecycle()
+  when (state) {
+    MainViewModel.UiState.Loading -> CircularProgressIndicator()
+    MainViewModel.UiState.SignedIn -> {}
+
+    MainViewModel.UiState.SignedOut -> AuthStartView(authMode = AuthMode.SignIn)
+  }
 }
 
 @PreviewLightDark
 @Composable
 private fun PreviewMainContent() {
-  WorkbenchTheme { MainContent() }
+  WorkbenchTheme {}
 }
