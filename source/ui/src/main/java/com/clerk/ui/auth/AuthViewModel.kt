029412201e12ab02205e09a065b3a94b412f2033
Sam Wolfand <sam.wolfand@gmail.com>
2025-09-18 16:17:41 -0700

    create sign in function

--- source/ui/src/main/java/com/clerk/ui/auth/AuthViewModel.kt
+++ source/ui/src/main/java/com/clerk/ui/auth/AuthViewModel.kt
@@ -2,7 +2,12 @@ package com.clerk.ui.auth
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.clerk.api.network.serialization.longErrorMessageOrNull
+import com.clerk.api.network.serialization.onFailure
+import com.clerk.api.network.serialization.onSuccess
 import com.clerk.api.signin.SignIn
+import com.clerk.api.signin.prepareFirstFactor
+import com.clerk.api.signin.startingFirstFactor
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -12,20 +17,61 @@ internal class AuthViewModel : ViewModel() {
   private val _state = MutableStateFlow<AuthState>(AuthState.Idle)
   val state = _state.asStateFlow()
 
-  fun startAuth(authMode: AuthMode) {
+  fun startAuth(
+    authMode: AuthMode,
+    isPhoneNumberFieldActive: Boolean,
+    phoneNumber: String,
+    identifier: String,
+  ) {
     when (authMode) {
-      AuthMode.SignIn -> TODO()
+      AuthMode.SignIn ->
+        signIn(
+          isPhoneNumberFieldActive = isPhoneNumberFieldActive,
+          phoneNumber = phoneNumber,
+          identifier = identifier,
+        )
       AuthMode.SignUp -> TODO()
       AuthMode.SignInOrUp -> TODO()
     }
   }
 
-  fun signIn(isPhoneNumberFieldActive: Boolean, phoneNumber: String, identifier: String) {
-    _state.value = AuthState.Loading
-    val identifier = if (isPhoneNumberFieldActive) phoneNumber else identifier
-    viewModelScope.launch {
-      SignIn.create(SignIn.CreateParams.Strategy.Identifier(identifier = identifier))
+  private fun signIn(isPhoneNumberFieldActive: Boolean, phoneNumber: String, identifier: String) {
+    executeWithState {
+      val resolvedIdentifier = if (isPhoneNumberFieldActive) phoneNumber else identifier
+
+      SignIn.create(SignIn.CreateParams.Strategy.Identifier(identifier = resolvedIdentifier))
+        .onSuccess { signIn -> handleSignInSuccess(signIn) }
+        .onFailure { throwable -> _state.value = AuthState.Error(throwable.longErrorMessageOrNull) }
     }
+  }
+
+  private suspend fun handleSignInSuccess(signIn: SignIn) {
+    when {
+      signIn.requiresEnterpriseSSO() -> handleEnterpriseSSO(signIn)
+      else -> _state.value = AuthState.Success(signIn)
+    }
+  }
+
+  private suspend fun handleEnterpriseSSO(signIn: SignIn) {
+    signIn
+      .prepareFirstFactor(SignIn.PrepareFirstFactorParams.EnterpriseSSO())
+      .onSuccess {
+        val redirectUrl = signIn.getExternalVerificationRedirectUrl()
+        authenticateWithEnterpriseSSO(redirectUrl)
+      }
+      .onFailure { throwable -> _state.value = AuthState.Error(throwable.longErrorMessageOrNull) }
+  }
+
+  private suspend fun authenticateWithEnterpriseSSO(redirectUrl: String) {
+    SignIn.authenticateWithRedirect(
+      SignIn.AuthenticateWithRedirectParams.EnterpriseSSO(redirectUrl = redirectUrl)
+    )
+    TODO("Need to understand `authenticateWithRedirect` from the iOS SDK")
+  }
+
+  private fun executeWithState(block: suspend () -> Unit) {
+    _state.value = AuthState.Loading
+    viewModelScope.launch { block() }
   }
 
   sealed interface AuthState {
@@ -33,8 +79,15 @@ internal class AuthViewModel : ViewModel() {
 
     object Loading : AuthState
 
-    object Success : AuthState
+    data class Success(val signIn: SignIn) : AuthState
 
     data class Error(val message: String?) : AuthState
   }
 }
+
+private fun SignIn.requiresEnterpriseSSO(): Boolean =
+  startingFirstFactor?.strategy == "enterprise_sso"
+
+private fun SignIn.getExternalVerificationRedirectUrl(): String =
+  firstFactorVerification?.externalVerificationRedirectUrl
+    ?: error("External verification redirect URL is null")
