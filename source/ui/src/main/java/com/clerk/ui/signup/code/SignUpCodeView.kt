ddc54bf6c4ee610bb8068b75ab5321015439c199
Sam Wolfand <sam.wolfand@gmail.com>
2025-09-19 20:57:14 -0700

    add more text states

--- source/ui/src/main/java/com/clerk/ui/signup/code/SignUpCodeView.kt
+++ source/ui/src/main/java/com/clerk/ui/signup/code/SignUpCodeView.kt
@@ -1,20 +1,29 @@
-package com.clerk.ui.signup
+package com.clerk.ui.signup.code
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.clerk.ui.R
 import com.clerk.ui.core.common.ClerkThemedAuthScaffold
 import com.clerk.ui.core.common.Spacers
 import com.clerk.ui.core.input.ClerkCodeInputField
 import com.clerk.ui.core.progress.ClerkLinearProgressIndicator
+import com.clerk.ui.util.formattedAsPhoneNumberIfPossible
 
 @Composable
 fun SignUpCodeView(field: Field, modifier: Modifier = Modifier) {
+  val title =
+    when (field) {
+      is Field.Phone -> stringResource(R.string.check_your_phone)
+      is Field.Email -> stringResource(R.string.check_your_email)
+    }
+
   ClerkThemedAuthScaffold(
     modifier = modifier,
-    title = "Check your email",
+    title = title,
     hasLogo = false,
-    identifier = field.value,
+    identifier = field.value.formattedAsPhoneNumberIfPossible,
   ) {
     ClerkLinearProgressIndicator(progress = 0)
     Spacers.Vertical.Spacer32()
@@ -25,7 +34,7 @@ fun SignUpCodeView(field: Field, modifier: Modifier = Modifier) {
 @PreviewLightDark
 @Composable
 private fun Preview() {
-  SignUpCodeView(field = Field.Email("example@email.com"))
+  SignUpCodeView(field = Field.Phone("3012370655"))
 }
 
 sealed interface Field {
