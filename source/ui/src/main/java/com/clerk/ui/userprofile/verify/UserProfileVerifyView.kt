b0a1f284f63fb1313834790b48b6cb0e5e64127d
Sam Wolfand <sam.wolfand@gmail.com>
2025-10-06 17:50:18 -0700

    add UserProfileVerifyViewModel.kt

--- source/ui/src/main/java/com/clerk/ui/userprofile/verify/UserProfileVerifyView.kt
+++ source/ui/src/main/java/com/clerk/ui/userprofile/verify/UserProfileVerifyView.kt
@@ -3,19 +3,37 @@ package com.clerk.ui.userprofile.verify
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.clerk.api.emailaddress.EmailAddress
 import com.clerk.api.phonenumber.PhoneNumber
 import com.clerk.ui.R
 import com.clerk.ui.core.input.ClerkCodeInputField
 import com.clerk.ui.core.scaffold.ClerkThemedProfileScaffold
 import com.clerk.ui.core.spacers.Spacers
+import com.clerk.ui.signin.code.VerificationState as CodeVerificationState
 import com.clerk.ui.theme.ClerkMaterialTheme
 
 @Composable
 fun UserProfileVerifyView(mode: Mode, modifier: Modifier = Modifier) {
+  UserProfileVerifyViewImpl(mode = mode, modifier = modifier)
+}
+
+@Composable
+private fun UserProfileVerifyViewImpl(
+  mode: Mode,
+  modifier: Modifier = Modifier,
+  viewModel: UserProfileVerifyViewModel = viewModel(),
+) {
+
+  val state by viewModel.state.collectAsStateWithLifecycle()
+  val verificationTextState by viewModel.verificationTextState.collectAsStateWithLifecycle()
+  LaunchedEffect(mode) { prepareCode(mode, viewModel) }
 
   ClerkThemedProfileScaffold(
     modifier = modifier,
@@ -28,7 +46,38 @@ fun UserProfileVerifyView(mode: Mode, modifier: Modifier = Modifier) {
       color = ClerkMaterialTheme.colors.mutedForeground,
     )
     Spacers.Vertical.Spacer24()
-    ClerkCodeInputField(onTextChange = {}, onClickResend = {}, showResend = mode.showResend())
+    ClerkCodeInputField(
+      onTextChange = {
+        if (it.length == 6) {
+          when (mode) {
+            is Mode.Email -> viewModel.attemptEmailAddress(mode.emailAddress, it)
+            is Mode.Phone -> viewModel.attemptPhoneNumber(mode.phoneNumber, it)
+            Mode.Totp -> viewModel.attemptTotp(it)
+          }
+        }
+      },
+      onClickResend = { prepareCode(mode, viewModel) },
+      showResend = mode.showResend(),
+      verificationState = verificationTextState.getTextVerificationState(),
+    )
+  }
+}
+
+private fun UserProfileVerifyViewModel.VerificationTextState.getTextVerificationState():
+  CodeVerificationState {
+  return when (this) {
+    UserProfileVerifyViewModel.VerificationTextState.Default -> CodeVerificationState.Default
+    is UserProfileVerifyViewModel.VerificationTextState.Error -> CodeVerificationState.Error
+    UserProfileVerifyViewModel.VerificationTextState.Verified -> CodeVerificationState.Success
+    UserProfileVerifyViewModel.VerificationTextState.Verifying -> CodeVerificationState.Verifying
+  }
+}
+
+private fun prepareCode(mode: Mode, viewModel: UserProfileVerifyViewModel) {
+  when (mode) {
+    is Mode.Email -> viewModel.prepareEmailAddress(mode.emailAddress)
+    is Mode.Phone -> viewModel.preparePhoneNumber(mode.phoneNumber)
+    Mode.Totp -> {}
   }
 }
 
@@ -38,16 +87,6 @@ private fun Preview() {
   UserProfileVerifyView(
     mode = Mode.Email(emailAddress = EmailAddress(id = "id", emailAddress = "user@email.com"))
   )
-}
-
-sealed interface VerificationState {
-  data object Default : VerificationState
-
-  data object Verifying : VerificationState
-
-  data object Success : VerificationState
-
-  data class Error(val message: String) : VerificationState
 }
 
 private fun Mode.showResend(): Boolean {
---
+++ source/ui/src/main/java/com/clerk/ui/userprofile/verify/UserProfileVerifyViewModel.kt
@@ -0,0 +1,111 @@
+package com.clerk.ui.userprofile.verify
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.clerk.api.emailaddress.EmailAddress
+import com.clerk.api.emailaddress.attemptVerification
+import com.clerk.api.emailaddress.prepareVerification
+import com.clerk.api.network.serialization.longErrorMessageOrNull
+import com.clerk.api.network.serialization.onFailure
+import com.clerk.api.network.serialization.onSuccess
+import com.clerk.api.phonenumber.PhoneNumber
+import com.clerk.api.phonenumber.attemptVerification
+import com.clerk.api.phonenumber.prepareVerification
+import com.clerk.api.user.attemptTOTPVerification
+import com.clerk.ui.core.common.guardUser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+internal class UserProfileVerifyViewModel : ViewModel() {
+
+  private val _state = MutableStateFlow<AuthState>(AuthState.Idle)
+  val state = _state.asStateFlow()
+
+  private val _verificationTextState =
+    MutableStateFlow<VerificationTextState>(VerificationTextState.Default)
+  val verificationTextState = _verificationTextState.asStateFlow()
+
+  fun preparePhoneNumber(phoneNumber: PhoneNumber) {
+    _state.value = AuthState.Loading
+    viewModelScope.launch(Dispatchers.IO) {
+      phoneNumber
+        .prepareVerification()
+        .onSuccess { _state.value = AuthState.Success }
+        .onFailure { _state.value = AuthState.Error(it.longErrorMessageOrNull) }
+    }
+  }
+
+  fun prepareEmailAddress(emailAddress: EmailAddress) {
+    _state.value = AuthState.Loading
+    viewModelScope.launch(Dispatchers.IO) {
+      emailAddress
+        .prepareVerification(EmailAddress.PrepareVerificationParams.EmailCode())
+        .onSuccess { _state.value = AuthState.Success }
+        .onFailure { _state.value = AuthState.Error(it.longErrorMessageOrNull) }
+    }
+  }
+
+  fun attemptEmailAddress(emailAddress: EmailAddress, code: String) {
+    _verificationTextState.value = VerificationTextState.Verifying
+    viewModelScope.launch(Dispatchers.IO) {
+      emailAddress
+        .attemptVerification(code)
+        .onSuccess { _verificationTextState.value = VerificationTextState.Verified }
+        .onFailure {
+          _verificationTextState.value = VerificationTextState.Error(it.longErrorMessageOrNull)
+        }
+    }
+  }
+
+  fun attemptPhoneNumber(phoneNumber: PhoneNumber, code: String) {
+    _verificationTextState.value = VerificationTextState.Verifying
+    viewModelScope.launch(Dispatchers.IO) {
+      phoneNumber
+        .attemptVerification(code)
+        .onSuccess { _verificationTextState.value = VerificationTextState.Verified }
+        .onFailure {
+          _verificationTextState.value = VerificationTextState.Error(it.longErrorMessageOrNull)
+        }
+    }
+  }
+
+  fun attemptTotp(code: String) {
+    _verificationTextState.value = VerificationTextState.Verifying
+    guardUser(
+      userDoesNotExist = {
+        _verificationTextState.value = VerificationTextState.Error("User does not exist")
+      }
+    ) { user ->
+      viewModelScope.launch(Dispatchers.IO) {
+        user
+          .attemptTOTPVerification(code)
+          .onSuccess { _verificationTextState.value = VerificationTextState.Verified }
+          .onFailure {
+            _verificationTextState.value = VerificationTextState.Error(it.longErrorMessageOrNull)
+          }
+      }
+    }
+  }
+
+  sealed interface VerificationTextState {
+    data object Default : VerificationTextState
+
+    data object Verifying : VerificationTextState
+
+    data object Verified : VerificationTextState
+
+    data class Error(val error: String?) : VerificationTextState
+  }
+
+  sealed interface AuthState {
+    object Idle : AuthState
+
+    object Loading : AuthState
+
+    data object Success : AuthState
+
+    data class Error(val error: String?) : AuthState
+  }
+}
