787743aa4c5253b149b0b9aebab92a2fbc710f0d
Sam Wolfand <sam.wolfand@gmail.com>
2025-10-08 19:40:10 -0700

    everything except button

---
+++ source/ui/src/main/java/com/clerk/ui/userprofile/security/device/UserProfileDeviceRow.kt
@@ -0,0 +1,150 @@
+package com.clerk.ui.userprofile.security.device
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImage
+import com.clerk.api.Clerk
+import com.clerk.api.session.Session
+import com.clerk.api.session.SessionActivity
+import com.clerk.api.session.isThisDevice
+import com.clerk.ui.R
+import com.clerk.ui.core.badge.Badge
+import com.clerk.ui.core.badge.ClerkBadgeType
+import com.clerk.ui.core.dimens.dp16
+import com.clerk.ui.core.dimens.dp24
+import com.clerk.ui.core.dimens.dp4
+import com.clerk.ui.core.extensions.browserFormatted
+import com.clerk.ui.core.extensions.deviceImage
+import com.clerk.ui.core.extensions.deviceText
+import com.clerk.ui.core.extensions.ipAndLocationFormatted
+import com.clerk.ui.core.extensions.lastActiveRelativeTime
+import com.clerk.ui.theme.ClerkMaterialTheme
+
+@Composable
+fun UserProfileDeviceRow(modifier: Modifier = Modifier) {
+  val session by Clerk.sessionFlow.collectAsStateWithLifecycle()
+  UserProfileDeviceRowImpl(session = session!!, modifier = modifier)
+}
+
+@Composable
+private fun UserProfileDeviceRowImpl(
+  session: Session,
+  modifier: Modifier = Modifier,
+  forceIsThisDevice: Boolean = false,
+) {
+  session.latestActivity?.let { activity ->
+    ClerkMaterialTheme {
+      Row(
+        modifier =
+          Modifier.fillMaxWidth()
+            .background(color = ClerkMaterialTheme.colors.background)
+            .padding(vertical = dp16, horizontal = dp24)
+            .then(modifier)
+      ) {
+        DeviceInfoWithIcon(activity, session, forceIsThisDevice)
+        Spacer(modifier = Modifier.weight(1f))
+        IconButton(modifier = Modifier.align(Alignment.CenterVertically), onClick = {}) {
+          Icon(
+            imageVector = Icons.Outlined.MoreVert,
+            contentDescription = stringResource(R.string.more_options),
+            tint = ClerkMaterialTheme.colors.mutedForeground,
+          )
+        }
+      }
+    }
+  }
+}
+
+@Composable
+private fun DeviceInfoWithIcon(
+  activity: SessionActivity,
+  session: Session,
+  forceIsThisDevice: Boolean,
+) {
+  Row(
+    horizontalArrangement = Arrangement.spacedBy(dp16, alignment = Alignment.Start),
+    verticalAlignment = Alignment.Top,
+  ) {
+    AsyncImage(
+      modifier = Modifier.size(dp24),
+      model = activity.deviceImage(),
+      contentDescription = null,
+      fallback = painterResource(R.drawable.ic_mobile),
+    )
+    Column {
+      Row(
+        horizontalArrangement = Arrangement.spacedBy(dp4, alignment = Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
+      ) {
+        Text(
+          text = stringResource(activity.deviceText()),
+          color = ClerkMaterialTheme.colors.foreground,
+          style = ClerkMaterialTheme.typography.bodyLarge,
+        )
+        if (session.isThisDevice || forceIsThisDevice) {
+          Badge(text = stringResource(R.string.this_device), badgeType = ClerkBadgeType.Secondary)
+        }
+      }
+      Text(
+        text = activity.browserFormatted,
+        style = ClerkMaterialTheme.typography.bodyMedium,
+        color = ClerkMaterialTheme.colors.mutedForeground,
+      )
+      Text(
+        text = activity.ipAndLocationFormatted,
+        style = ClerkMaterialTheme.typography.bodyMedium,
+        color = ClerkMaterialTheme.colors.mutedForeground,
+      )
+      Text(
+        text = session.lastActiveRelativeTime,
+        style = ClerkMaterialTheme.typography.bodyMedium,
+        color = ClerkMaterialTheme.colors.mutedForeground,
+      )
+    }
+  }
+}
+
+@PreviewLightDark
+@Composable
+private fun Preview() {
+  UserProfileDeviceRowImpl(
+    forceIsThisDevice = true,
+    session =
+      Session(
+        id = "123456",
+        expireAt = 1759976778801,
+        lastActiveAt = 1759976778801,
+        createdAt = 1759976778801,
+        updatedAt = 1759976778801,
+        latestActivity =
+          SessionActivity(
+            id = "activity_123",
+            ipAddress = "196.172.122.88",
+            isMobile = true,
+            browserName = "Chrome",
+            browserVersion = "139.0.0.0",
+            city = "San Francisco",
+            country = "CA",
+          ),
+      ),
+  )
+}
