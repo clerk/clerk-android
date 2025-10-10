18528224d940d5e8f26ae2923764cf66dea32b18
Sam Wolfand <sam.wolfand@gmail.com>
2025-10-10 16:01:47 -0700

    add custom text button padding

--- source/ui/src/main/java/com/clerk/ui/core/button/standard/ClerkTextButton.kt
+++ source/ui/src/main/java/com/clerk/ui/core/button/standard/ClerkTextButton.kt
@@ -1,16 +1,22 @@
 package com.clerk.ui.core.button.standard
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.clerk.api.Clerk
 import com.clerk.api.ui.ClerkTheme
+import com.clerk.ui.core.dimens.dp6
 import com.clerk.ui.core.dimens.dp8
 import com.clerk.ui.theme.ClerkMaterialTheme
 import com.clerk.ui.theme.DefaultColors
@@ -34,10 +40,26 @@ fun ClerkTextButton(
   modifier: Modifier = Modifier,
   textColor: Color = ClerkMaterialTheme.colors.primary,
   textStyle: TextStyle = ClerkMaterialTheme.typography.titleSmall,
+  boundedRipple: Boolean = true,
+  rippleColor: Color = Color.Unspecified, // Unspecified -> uses LocalContentColor
   onClick: () -> Unit,
 ) {
+  val interaction = remember { MutableInteractionSource() }
+
   ClerkMaterialTheme {
-    Box(modifier = Modifier.padding(horizontal = dp8).then(modifier).clickable { onClick() }) {
+    Box(
+      modifier =
+        modifier
+          .padding(horizontal = dp8)
+          .clip(ClerkMaterialTheme.shape) // masks ripple to this shape
+          .clickable(
+            interactionSource = interaction,
+            indication = ripple(bounded = boundedRipple, color = rippleColor),
+            role = Role.Button,
+            onClick = onClick,
+          )
+          .padding(horizontal = dp8, vertical = dp6)
+    ) {
       Text(text = text, color = textColor, style = textStyle)
     }
   }
