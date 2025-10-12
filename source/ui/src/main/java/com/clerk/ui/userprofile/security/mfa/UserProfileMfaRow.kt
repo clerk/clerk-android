5de3d892c55cf973bb22db16fff5c0d72c1239cf
Sam Wolfand <sam.wolfand@gmail.com>
2025-10-11 19:44:36 -0700

    add text

--- source/ui/src/main/java/com/clerk/ui/userprofile/security/mfa/UserProfileMfaRow.kt
+++ source/ui/src/main/java/com/clerk/ui/userprofile/security/mfa/UserProfileMfaRow.kt
@@ -1,6 +1,8 @@
 package com.clerk.ui.userprofile.security.mfa
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,43 +11,96 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.clerk.api.Clerk
 import com.clerk.api.phonenumber.PhoneNumber
 import com.clerk.ui.R
+import com.clerk.ui.core.badge.Badge
+import com.clerk.ui.core.badge.ClerkBadgeType
+import com.clerk.ui.core.dimens.dp0
 import com.clerk.ui.core.dimens.dp16
 import com.clerk.ui.core.dimens.dp18
+import com.clerk.ui.core.dimens.dp2
 import com.clerk.ui.core.dimens.dp24
+import com.clerk.ui.core.dimens.dp4
 import com.clerk.ui.core.menu.DropDownItem
 import com.clerk.ui.core.menu.ItemMoreMenu
+import com.clerk.ui.core.spacers.Spacers
 import com.clerk.ui.theme.ClerkMaterialTheme
+import com.clerk.ui.util.formattedAsPhoneNumberIfPossible
 import kotlinx.collections.immutable.persistentListOf
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UserProfileMfaRow(style: Style, modifier: Modifier = Modifier) {
+fun UserProfileMfaRow(
+  style: Style,
+  modifier: Modifier = Modifier,
+  isDefault: Boolean = false,
+  title: String? = null,
+) {
+  val hasHeader = isDefault || title != null
+
   ClerkMaterialTheme {
     Row(
       modifier =
         Modifier.fillMaxWidth()
-          .background(color = ClerkMaterialTheme.colors.background)
-          .padding(horizontal = dp24)
-          .padding(vertical = dp16)
+          .background(ClerkMaterialTheme.colors.background)
+          .padding(horizontal = dp24, vertical = dp16)
           .then(modifier),
-      verticalAlignment = Alignment.Top,
+      horizontalArrangement = Arrangement.spacedBy(dp16),
+      verticalAlignment = Alignment.CenterVertically,
     ) {
-      Icon(
-        modifier = Modifier.size(dp18),
-        tint = ClerkMaterialTheme.colors.mutedForeground,
-        painter = painterResource(style.icon()),
-        contentDescription = null,
-      )
-      Column {}
-      Spacer(modifier = Modifier.weight(1f))
+      val padding = if (title != null) dp4 else dp0
+      if (hasHeader) {
+        Icon(
+          painter = painterResource(style.icon()),
+          tint = ClerkMaterialTheme.colors.mutedForeground,
+          contentDescription = null,
+          modifier = Modifier.padding(top = padding).size(dp18).align(Alignment.Top),
+        )
+      } else {
+        Box(
+          modifier = Modifier.size(dp18).align(Alignment.CenterVertically),
+          contentAlignment = Alignment.Center,
+        ) {
+          Icon(
+            painter = painterResource(style.icon()),
+            tint = ClerkMaterialTheme.colors.mutedForeground,
+            contentDescription = null,
+            modifier = Modifier.size(dp18),
+          )
+        }
+      }
+
+      Column(verticalArrangement = Arrangement.Center) {
+        if (isDefault) {
+          Badge(text = stringResource(R.string.default_text), badgeType = ClerkBadgeType.Secondary)
+          Spacers.Vertical.Spacer4()
+        }
+
+        title?.let {
+          Text(
+            text = it,
+            style = ClerkMaterialTheme.typography.bodyMedium,
+            color = ClerkMaterialTheme.colors.mutedForeground,
+          )
+        }
+
+        Text(
+          text = style.text(),
+          style = ClerkMaterialTheme.typography.bodyLarge,
+          color = ClerkMaterialTheme.colors.foreground,
+        )
+      }
+
+      Spacer(Modifier.weight(1f))
       ItemMoreMenu(
         dropDownItems =
           persistentListOf(
@@ -60,7 +115,7 @@ fun UserProfileMfaRow(style: Style, modifier: Modifier = Modifier) {
               textRes = R.string.set_as_default,
               isHidden =
                 style !is Style.Sms ||
-                  Clerk.user?.totpEnabled != true && !style.phoneNumber.defaultSecondFactor,
+                  (Clerk.user?.totpEnabled != true && !style.phoneNumber.defaultSecondFactor),
             ),
             DropDownItem(
               id = MfaAction.Regenerate,
@@ -68,19 +123,18 @@ fun UserProfileMfaRow(style: Style, modifier: Modifier = Modifier) {
               isHidden = style != Style.BackupCodes,
             ),
           ),
-        onClick = {
-          when (it) {
-            MfaAction.Remove -> {
-              if (style == Style.AuthenticatorApp) {}
-
-              if (style is Style.Sms) {}
-            }
-            MfaAction.SetAsDefault -> TODO()
-            MfaAction.Regenerate -> TODO()
-          }
-        },
+        onClick = { /* ... */ },
       )
     }
+  }
+}
+
+@Composable
+fun Style.text(): String {
+  return when (this) {
+    Style.AuthenticatorApp -> "Authenticator app"
+    is Style.Sms -> "SMS code ${this.phoneNumber.phoneNumber.formattedAsPhoneNumberIfPossible}"
+    Style.BackupCodes -> "Backup codes"
   }
 }
 
@@ -99,6 +153,7 @@ fun Style.icon(): Int {
   }
 }
 
+@Stable
 sealed interface Style {
   data object AuthenticatorApp : Style
 
@@ -110,5 +165,14 @@ sealed interface Style {
 @PreviewLightDark
 @Composable
 private fun Preview() {
-  ClerkMaterialTheme { Column { UserProfileMfaRow(style = Style.AuthenticatorApp) } }
+  ClerkMaterialTheme {
+    Column {
+      UserProfileMfaRow(style = Style.AuthenticatorApp, isDefault = true)
+      UserProfileMfaRow(
+        style = Style.Sms(phoneNumber = PhoneNumber(id = "1", phoneNumber = "+13012370655")),
+        title = "Primary",
+      )
+      UserProfileMfaRow(style = Style.BackupCodes)
+    }
+  }
 }
---
+++ source/ui/src/main/java/com/clerk/ui/userprofile/security/mfa/UserProfileMfaViewModel.kt
@@ -0,0 +1,8 @@
+package com.clerk.ui.userprofile.security.mfa
+
+import androidx.lifecycle.ViewModel
+
+class UserProfileMfaViewModel: ViewModel() {
+
+
+}
