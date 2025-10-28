38ef7aeb284bbfc53ea02a614683705b6edc661c
Sam Wolfand <sam.wolfand@gmail.com>
2025-10-28 15:46:18 -0700

    add ui for renaming password

---
+++ source/ui/src/main/java/com/clerk/ui/userprofile/security/passkey/UserProfilePasskeyRenameView.kt
@@ -0,0 +1,73 @@
+package com.clerk.ui.userprofile.security.passkey
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.sp
+import com.clerk.ui.R
+import com.clerk.ui.core.button.standard.ClerkButton
+import com.clerk.ui.core.input.ClerkTextField
+import com.clerk.ui.core.scaffold.ClerkThemedProfileScaffold
+import com.clerk.ui.core.spacers.Spacers
+import com.clerk.ui.theme.ClerkMaterialTheme
+
+@Composable
+fun UserProfilePasskeyRenameView(
+  passkeyName: String,
+  modifier: Modifier = Modifier,
+  onBackPressed: () -> Unit,
+) {
+  UserProfilePasskeyRenameViewImpl(
+    modifier = modifier,
+    passkeyName = passkeyName,
+    onBackPressed = onBackPressed,
+  )
+}
+
+@Composable
+fun UserProfilePasskeyRenameViewImpl(
+  passkeyName: String,
+  modifier: Modifier = Modifier,
+  onBackPressed: () -> Unit,
+) {
+  var passkeyName by rememberSaveable { mutableStateOf(passkeyName) }
+  ClerkThemedProfileScaffold(
+    modifier = modifier,
+    hasBackButton = true,
+    title = stringResource(R.string.rename_passkey),
+    onBackPressed = onBackPressed,
+  ) {
+    Text(
+      text = stringResource(R.string.you_can_change_the_passkey_name),
+      style = ClerkMaterialTheme.typography.bodyMedium.copy(fontSize = 15.sp),
+      color = ClerkMaterialTheme.colors.mutedForeground,
+    )
+    Spacers.Vertical.Spacer20()
+    ClerkTextField(
+      value = passkeyName,
+      onValueChange = { passkeyName = it },
+      label = stringResource(R.string.name_of_passkey),
+    )
+    Spacers.Vertical.Spacer24()
+    ClerkButton(
+      modifier = Modifier.fillMaxWidth(),
+      text = stringResource(R.string.save),
+      onClick = {},
+    )
+  }
+}
+
+@PreviewLightDark
+@Composable
+private fun Preview() {
+  ClerkMaterialTheme {
+    UserProfilePasskeyRenameView(passkeyName = "One password", onBackPressed = {})
+  }
+}
