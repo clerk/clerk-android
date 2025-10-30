325b08086564f826394bb2c8b07f8360a7f4d5cb
Sam Wolfand <sam.wolfand@gmail.com>
2025-10-29 17:24:52 -0700

    update snapshots

--- source/ui/src/test/java/com/clerk/snapshot/userprofile/BackupCodeViewSnapshotTest.kt
+++ source/ui/src/test/java/com/clerk/snapshot/userprofile/BackupCodeViewSnapshotTest.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation3.runtime.rememberNavBackStack
 import com.clerk.api.Clerk
 import com.clerk.api.ui.ClerkTheme
 import com.clerk.base.BaseSnapshotTest
 import com.clerk.ui.theme.ClerkMaterialTheme
 import com.clerk.ui.theme.DefaultColors
+import com.clerk.ui.userprofile.UserProfileDestination
+import com.clerk.ui.userprofile.UserProfileStateProvider
 import com.clerk.ui.userprofile.security.BackupCodesView
-import com.clerk.ui.userprofile.security.MfaType
 import kotlinx.collections.immutable.persistentListOf
 import org.junit.Test
 
@@ -19,24 +21,27 @@ class BackupCodeViewSnapshotTest : BaseSnapshotTest() {
   @Test
   fun backupCodeViewSnapshotTestLight() {
     paparazzi.snapshot {
-      Box(modifier = Modifier.height(700.dp)) {
-        ClerkMaterialTheme {
-          BackupCodesView(
-            mfaType = MfaType.AuthenticatorApp,
-            codes =
-              persistentListOf(
-                "jsdwz752",
-                "abxkq983",
-                "abxkq983",
-                "mpltk294",
-                "mpltk294",
-                "qwert678",
-                "dj2b5ugx",
-                "xyztj501",
-                "qwert678",
-                "4nb52vql",
-              ),
-          )
+      val backStack =
+        rememberNavBackStack(UserProfileDestination.BackupCodeView(codes = emptyList()))
+      UserProfileStateProvider(backStack) {
+        Box(modifier = Modifier.height(700.dp)) {
+          ClerkMaterialTheme {
+            BackupCodesView(
+              codes =
+                persistentListOf(
+                  "jsdwz752",
+                  "abxkq983",
+                  "abxkq983",
+                  "mpltk294",
+                  "mpltk294",
+                  "qwert678",
+                  "dj2b5ugx",
+                  "xyztj501",
+                  "qwert678",
+                  "4nb52vql",
+                )
+            )
+          }
         }
       }
     }
@@ -46,24 +51,27 @@ class BackupCodeViewSnapshotTest : BaseSnapshotTest() {
   fun backupCodeViewSnapshotTestDark() {
     paparazzi.snapshot {
       Clerk.customTheme = ClerkTheme(colors = DefaultColors.dark)
-      Box(modifier = Modifier.height(700.dp)) {
-        ClerkMaterialTheme {
-          BackupCodesView(
-            mfaType = MfaType.AuthenticatorApp,
-            codes =
-              persistentListOf(
-                "jsdwz752",
-                "abxkq983",
-                "abxkq983",
-                "mpltk294",
-                "mpltk294",
-                "qwert678",
-                "dj2b5ugx",
-                "xyztj501",
-                "qwert678",
-                "4nb52vql",
-              ),
-          )
+      val backStack =
+        rememberNavBackStack(UserProfileDestination.BackupCodeView(codes = emptyList()))
+      UserProfileStateProvider(backStack) {
+        Box(modifier = Modifier.height(700.dp)) {
+          ClerkMaterialTheme {
+            BackupCodesView(
+              codes =
+                persistentListOf(
+                  "jsdwz752",
+                  "abxkq983",
+                  "abxkq983",
+                  "mpltk294",
+                  "mpltk294",
+                  "qwert678",
+                  "dj2b5ugx",
+                  "xyztj501",
+                  "qwert678",
+                  "4nb52vql",
+                )
+            )
+          }
         }
       }
     }
