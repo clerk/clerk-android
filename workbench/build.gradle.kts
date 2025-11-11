5e43395ad35dd48b8cc91bca054a2338e6069061
Sam Wolfand <sam.wolfand@gmail.com>
2025-11-11 10:52:50 -0800

    update workbench

--- workbench/build.gradle.kts
+++ workbench/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 dependencies {
   implementation(platform(libs.compose.bom))
   implementation(libs.activity.compose)
+  implementation(libs.androidx.compose.icons)
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime)
   implementation(libs.androidx.ui)
