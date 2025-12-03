0de6aabe657078375c9f319df2e32f861db53109
Sam Wolfand <sam.wolfand@gmail.com>
2025-12-03 10:13:42 -0600

    add is branded

--- source/ui/src/main/java/com/clerk/ui/core/footer/SecuredByClerkView.kt
+++ source/ui/src/main/java/com/clerk/ui/core/footer/SecuredByClerkView.kt
@@ -10,25 +10,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.clerk.api.Clerk
 import com.clerk.ui.R
 import com.clerk.ui.core.dimens.dp8
 import com.clerk.ui.theme.ClerkMaterialTheme
 
 @Composable
 internal fun SecuredByClerkView(modifier: Modifier = Modifier) {
-  ClerkMaterialTheme {
-    Row(
-      modifier = Modifier.then(modifier),
-      horizontalArrangement = Arrangement.spacedBy(dp8, alignment = Alignment.CenterHorizontally),
-      verticalAlignment = Alignment.CenterVertically,
-    ) {
-      Text(
-        text = "Secured by",
-        style = ClerkMaterialTheme.typography.bodyMedium,
-        color = ClerkMaterialTheme.colors.mutedForeground,
-      )
-      Image(painter = painterResource(R.drawable.ic_clerk_logo), contentDescription = "Clerk")
+  if (Clerk.isBranded) {
+    ClerkMaterialTheme {
+      Row(
+        modifier = Modifier.then(modifier),
+        horizontalArrangement = Arrangement.spacedBy(dp8, alignment = Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
+      ) {
+        Text(
+          text = stringResource(R.string.secured_by),
+          style = ClerkMaterialTheme.typography.bodyMedium,
+          color = ClerkMaterialTheme.colors.mutedForeground,
+        )
+        Image(painter = painterResource(R.drawable.ic_clerk_logo), contentDescription = "Clerk")
+      }
     }
   }
 }
