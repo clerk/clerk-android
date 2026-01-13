b5694c69d51f4f6e923a9942c07a5e41266969ad
Sam Wolfand <sam.wolfand@gmail.com>
2026-01-13 13:00:44 -0800

    refactor SignIn.kt to remove default values from EmailCode and PhoneCode data classes

--- source/api/src/main/kotlin/com/clerk/api/signin/SignIn.kt
+++ source/api/src/main/kotlin/com/clerk/api/signin/SignIn.kt
@@ -395,34 +395,28 @@ data class SignIn(
     @AutoMap
     @Serializable
     data class EmailCode(
-      @SerialName("email_address_id")
-      val emailAddressId: String =
-        signIn?.supportedFirstFactors!!.find { it.strategy == EMAIL_CODE }?.emailAddressId!!,
+      @SerialName("email_address_id") val emailAddressId: String,
       override val strategy: String = EMAIL_CODE,
     ) : PrepareFirstFactorParams
 
     @AutoMap
     @Serializable
     data class PhoneCode(
-      @SerialName("phone_number_id")
-      val phoneNumberId: String =
-        signIn?.supportedFirstFactors!!.find { it.strategy == PHONE_CODE }!!.phoneNumberId!!,
+      @SerialName("phone_number_id") val phoneNumberId: String,
       override val strategy: String = PHONE_CODE,
     ) : PrepareFirstFactorParams
 
     @AutoMap
     @Serializable
     data class ResetPasswordEmailCode(
-      val emailAddressId: String =
-        signIn?.supportedFirstFactors!!.find { it.strategy == EMAIL_CODE }?.emailAddressId!!,
+      val emailAddressId: String,
       override val strategy: String = RESET_PASSWORD_EMAIL_CODE,
     ) : PrepareFirstFactorParams
 
     @AutoMap
     @Serializable
     data class ResetPasswordPhoneCode(
-      val phoneNumberId: String =
-        signIn?.supportedFirstFactors!!.find { it.strategy == PHONE_CODE }!!.phoneNumberId!!,
+      val phoneNumberId: String,
       override val strategy: String = RESET_PASSWORD_PHONE_CODE,
     ) : PrepareFirstFactorParams
 
