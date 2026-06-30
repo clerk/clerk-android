0be0099003731db015d7aefe844b33565a395f2b
sam <sam@clerk.dev>
2026-06-29 18:19:19 -0700

    remove green

--- workbench/src/main/java/com/clerk/workbench/WorkbenchApplication.kt
+++ workbench/src/main/java/com/clerk/workbench/WorkbenchApplication.kt
@@ -1,11 +1,8 @@
 package com.clerk.workbench
 
 import android.app.Application
-import androidx.compose.ui.graphics.Color
 import com.clerk.api.Clerk
 import com.clerk.api.ClerkConfigurationOptions
-import com.clerk.api.ui.ClerkColors
-import com.clerk.api.ui.ClerkTheme
 
 class WorkbenchApplication : Application() {
 
@@ -20,7 +17,6 @@ class WorkbenchApplication : Application() {
         this,
         key,
         options = ClerkConfigurationOptions(enableDebugMode = true, proxyUrl = proxyUrl),
-        theme = ClerkTheme(colors = ClerkColors(background = Color.Green))
       )
     }
   }
